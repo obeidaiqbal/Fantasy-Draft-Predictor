5d45ac3202850fbb6e1e167595060e64da65eea7
Obeida Iqbal <obeidaiqbal41@gmail.com>
2025-08-28 14:05:27 -0400

    Added get_fantasy_points() function that adds a total fantasy points (FPTS) column to the dataframe

--- main.py
+++ main.py
@@ -16,10 +16,16 @@ def clean_data(df, season):
     columns = ["Player", "Age", "Team", "Pos", "PTS", "FG%", "3P%", "FT%", "AST", "TRB", "STL", "BLK", "TOV", "G", "GS", "MP", "Season"]
     return df[columns]
 
+def get_fantasy_points(df):
+    df["FPTS"] = (df["PTS"] + 1.5 * df["TRB"] + 2 * df["AST"] + 2 * df["STL"] + 2 * df["BLK"] - df["TOV"])
+    return df
+
 def main():
     df24, df25 = load_data()
     df24 = clean_data(df24, 24)
     df25 = clean_data(df25, 25)
+    df24 = get_fantasy_points(df24)
+    df25 = get_fantasy_points(df24)
     print(df24.head())
     print(df25.head())
 
