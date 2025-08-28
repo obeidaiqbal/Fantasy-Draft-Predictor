2ddccb503a94fc37eee0e426c802c6ffc5b92624
Obeida Iqbal <obeidaiqbal41@gmail.com>
2025-08-28 12:00:48 -0400

    Pointed csv reader to read files from data folder

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 
 def load_data():
-    df24 = pd.read_csv("2024season.csv")
-    df25 = pd.read_csv("2025season.csv")
+    df24 = pd.read_csv("data/2024season.csv")
+    df25 = pd.read_csv("data/2025season.csv")
     return df24, df25
 
 def clean_data(df, season):
@@ -13,18 +13,14 @@ def clean_data(df, season):
             df[col] = pd.to_numeric(df[col])
         except:
             pass
-    columns = [
-        "Player", "Age", "Team", "Pos", "G", "GS", "MP",
-        "FG%", "3P%", "FT%",
-        "TRB", "AST", "STL", "BLK", "TOV", "PTS",
-        "Season"
-    ]
+    columns = ["Player", "Age", "Team", "Pos", "PTS", "FG%", "3P%", "FT%", "AST", "TRB", "STL", "BLK", "TOV", "G", "GS", "MP", "Season"]
     return df[columns]
 
 def main():
     df24, df25 = load_data()
     df24 = clean_data(df24, 24)
     df25 = clean_data(df25, 25)
+    print(df24.head())
     print(df25.head())
 
 if __name__=="__main__":
